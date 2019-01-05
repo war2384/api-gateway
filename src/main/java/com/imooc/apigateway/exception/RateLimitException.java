8485efa2750633ba4bf9558f1f3c8cbe56f32250
war <37655074@qq.com>
2019-01-05 16:57:52 +0800

    限流和pre post filter

---
+++ src/main/java/com/imooc/apigateway/exception/RateLimitException.java
@@ -0,0 +1,10 @@
+package com.imooc.apigateway.exception;
+
+/**
+ * Created by war on 2019/1/5.
+ */
+public class RateLimitException extends RuntimeException {
+
+    
+
+}
---
+++ src/main/java/com/imooc/apigateway/filter/RateLimiterFilter.java
@@ -0,0 +1,39 @@
+package com.imooc.apigateway.filter;
+
+import com.google.common.util.concurrent.RateLimiter;
+import com.imooc.apigateway.exception.RateLimitException;
+import com.netflix.zuul.ZuulFilter;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SERVLET_DETECTION_FILTER_ORDER;
+
+/**
+ * Created by war on 2019/1/5.
+ */
+public class RateLimiterFilter extends ZuulFilter{
+
+    private static final RateLimiter RETE_LIMITER = RateLimiter.create(100);
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return SERVLET_DETECTION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        if(!RETE_LIMITER.tryAcquire()){
+            throw new RateLimitException();
+        }
+        return null;
+    }
+}
