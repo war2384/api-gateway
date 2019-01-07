e49d05c9c7d941e3a7207c586873e14ea8e2a3c5
war2384 <19880907er>
2019-01-07 15:59:20 +0800

    完成权限校验-跨域配置

---
+++ src/main/java/com/imooc/apigateway/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.imooc.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+/**
+ * 跨域配置
+ * Created by weiyuhang on 2019/1/7.
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFileter(){
+        final CorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+
+        return new CorsFilter(source);
+    }
+
+}
